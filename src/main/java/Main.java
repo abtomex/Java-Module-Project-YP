25701dd130fc1f43bd9e8d44afa1b08a973c93c1
Dmitriy <dchechumaev@mail.ru>
2025-01-17 15:44:36 +0300

    Практическая работа по спринту 2. Работа над замечаничми. Вынес классы в отдельные файлы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
 
@@ -50,61 +46,3 @@ public class Main {
     }
 }
 
-class RacingCar {
-    String name;
-    int velocity;
-    int distance;
-
-    public RacingCar(String name, int velocity) {
-        this.name = name;
-        this.velocity = velocity;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RacingCar racingCar = (RacingCar) o;
-        return velocity == racingCar.velocity && Objects.equals(name, racingCar.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, velocity);
-    }
-}
-
-class Race {
-    final int RACE_DURATION = 24;
-    Set<RacingCar> racers;
-    RacingCar leader;
-
-    public Race(RacingCar[] racingCars) {
-        racers = new HashSet<>();
-        Collections.addAll(racers, racingCars);
-    }
-
-    public void start() {
-        System.out.println("Гонка началась!!!");
-
-        for (RacingCar car : racers) {
-            car.distance = car.velocity * RACE_DURATION;
-        }
-        System.out.println("Гонка завершена.");
-    }
-
-    public void checkResults() {
-        System.out.println("Подведение итогов");
-        int maxDistance = 0;
-        for (RacingCar car : racers) {
-            if (car.distance > maxDistance) {
-                maxDistance = car.distance;
-                leader = car;
-            }
-        }
-    }
-
-    public RacingCar getLeader() {
-        return leader;
-    }
-}
